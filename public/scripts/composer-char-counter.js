9c81ff579c67f6c95eed8296b2849acc57529329
Thai Do <thai.hl.do@gmail.com>
2020-09-09 14:15:29 -0700

    commentted out some useless code

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,14 +3,14 @@ $(document).ready(function() {
   //   console.log(event);
   // });
 
-  $('#tweet-text').on('keyup', function(e) {
-    console.log($("#tweet-text").val().length)
-    console.log('what is this', this); 
-  })
+  // $('#tweet-text').on('keyup', function(e) {
+  //   console.log($("#tweet-text").val().length)
+  //   console.log('what is this', this); 
+  // })
 
-  $(".new-tweet textarea").on('click', () => {
-    console.log(this); //The this keyword here refers to something else!
-  });
+  // $(".new-tweet textarea").on('click', () => {
+  //   console.log(this); //The this keyword here refers to something else!
+  // });
 
   // $("#tweet-text" ).on('keyup', function() {
   //   $(".counter" ).html(`${140 - this.value.length}`);
